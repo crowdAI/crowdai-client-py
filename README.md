d1b26511121cb016bca67e7f7aee2b240a14eb61
SP Mohanty <sharada.mohanty@epfl.ch>
2017-05-02 10:13:13 +0200

    Adding initial scaffolding

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # crowdai-client-py
+Python client to interact with CrowdAI Grading Server.
+
+# Author
+S.P. Mohanty <sharada.mohanty@epfl.ch>
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='crowdai',
+      version='0.1',
+      description='Python client to interact with CrowdAI Grading Server.',
+      url='http://github.com/storborg/funniest',
+      author='S.P. Mohanty',
+      author_email='sharada.mohanty@epfl.ch',
+      license='MIT',
+      packages=['crowdai'],
+      zip_safe=False)
