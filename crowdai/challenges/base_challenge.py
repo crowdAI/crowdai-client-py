f53681ba48471aa51edfbe6b0e5bc338f6576e55
SP Mohanty <sharada.mohanty@epfl.ch>
2017-05-04 12:49:03 +0200

    Implementation custom exception classes

--- crowdai/challenges/base_challenge.py
+++ crowdai/challenges/base_challenge.py
@@ -1,5 +1,11 @@
 from socketIO_client import SocketIO, LoggingNamespace
 
+class BaseChallengeException(Exception):
+    pass
+
+class CrowdAIAuthenticationError(BaseChallengeException):
+    pass
+
 class BaseChallenge(object):
     def __init__(self, challenge_id, api_key, config):
         self.api_key = api_key
@@ -11,9 +17,11 @@ class BaseChallenge(object):
         self.socketio = SocketIO(self.config['remote_host'], self.config['remote_port'], LoggingNamespace)
 
     def _authenticate_response(self, args):
-        self.session_key = args["session_token"]
-        print args
-        #TO-DO: Unify the naming convention
+        if args["status"] == True:
+            self.session_key = args["session_token"]
+        else:
+            raise CrowdAIAuthenticationError(args["message"])
+
     def _authenticate(self):
         print "Beginning Authenticating :", self.challenge_id, self.api_key
         self.socketio.emit('authenticate', {"API_KEY":self.api_key,
@@ -22,4 +30,6 @@ class BaseChallenge(object):
         self.socketio.wait_for_callbacks(seconds=self.config['TIMEOUT_AUTHENTICATE'])
         if self.session_key == None:
             print "Authentication timeout..."
+        else:
+            print "Authentication successful :: Token :: ", self.session_key
             # TO-DO: Raise Error
