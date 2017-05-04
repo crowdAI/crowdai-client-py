8f2c314996560a1b89ae361b22de88c9b2d46fa4
SP Mohanty <sharada.mohanty@epfl.ch>
2017-05-04 16:20:27 +0200

    Refactor execute_function

--- crowdai/challenges/base_challenge.py
+++ crowdai/challenges/base_challenge.py
@@ -6,6 +6,9 @@ class BaseChallengeException(Exception):
 class CrowdAIAuthenticationError(BaseChallengeException):
     pass
 
+class CrowdAIExecuteFunctionError(BaseChallengeException):
+    pass
+
 class BaseChallenge(object):
     def __init__(self, challenge_id, api_key, config):
         self.api_key = api_key
@@ -36,3 +39,27 @@ class BaseChallenge(object):
         else:
             print "Authentication successful :: Token :: ", self.session_key
             # TO-DO: Log authentication successful
+
+    def on_execute_function_response(self, args):
+        if args["status"] == True:
+            self.execute_function_response = args
+        else:
+            # TO-DO: Log Challenge Error
+            raise CrowdAIExecuteFunctionError(args["message"])
+
+    def execute_function(self, function_name, data, dry_run=False):
+        #TO-DO : Validate if authenticated
+        self.execute_function_response = None
+        self.socketio.emit('execute_function',
+                        {   "session_token": self.session_key,
+                            "challenge_id": self.challenge_id,
+                            "function_name": function_name,
+                            "data": data,
+                            "dry_run" : dry_run
+                        }, self.on_execute_function_response)
+        self.socketio.wait_for_callbacks(seconds=self.config['challenges'][self.challenge_id]["TIMEOUT_EXECUTION"])
+        if self.execute_function_response == None:
+            raise CrowdAIExecuteFunctionError("Evaluation Request Timeout")
+            # print "Evaluation Request Timed Out..."
+        else:
+            return self.execute_function_response["response"]
--- crowdai/challenges/gecco_optimization_challenge_2017.py
+++ crowdai/challenges/gecco_optimization_challenge_2017.py
@@ -1,8 +1,5 @@
 from base_challenge import BaseChallenge
 
-class GeccoOptimizationChallenge2017Error(Exception):
-    pass
-
 class GeccoOptimizationChallenge2017(BaseChallenge):
     def __init__(self, api_key, config):
         self.challenge_id = "GeccoOptimizationChallenge2017"
@@ -10,31 +7,8 @@ class GeccoOptimizationChallenge2017(BaseChallenge):
         self._connect()
         self._authenticate()
 
-    def _on_evaluate_response(self, args):
-        print "Evaluate Response : ",args
-        #TO-DO: Do Progress handling here
-        if args["status"] == True:
-            self.evaluation_response = args
-        else:
-            # TO-DO: Log Challenge Error
-            raise GeccoOptimizationChallenge2017Error(args["message"])
-
-
     def evaluate(self, data, dry_run=False):
-        self.evaluation_response = None
-        self.socketio.emit('execute_function',
-                        {   "session_token": self.session_key,
-                            "challenge_id": self.challenge_id,
-                            "function_name": "evaluate",
-                            "data": data,
-                            "dry_run" : dry_run
-                        }, self._on_evaluate_response)
-        self.socketio.wait_for_callbacks(seconds=self.config['challenges'][self.challenge_id]["TIMEOUT_EXECUTION"])
-        if self.evaluation_response == None:
-            raise GeccoOptimizationChallenge2017Error("Evaluation Request Timeout")
-            # print "Evaluation Request Timed Out..."
-        else:
-            return self.evaluation_response
+        return self.execute_function('evaluate', data)
 
     def submit(self, data, dry_run=False):
         #Submit final score
