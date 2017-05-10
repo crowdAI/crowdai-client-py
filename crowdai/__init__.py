35d4e1dde6f1cfe90c2298c06a5f555b9b2b4230
SP Mohanty <sharada.mohanty@epfl.ch>
2017-05-10 08:32:02 +0200

    Make sure that the firt thing that happens is for the folder path to be added to the sys path

--- crowdai/__init__.py
+++ crowdai/__init__.py
@@ -1,11 +1,8 @@
-from .config import config
-
 import sys
 import os
-
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
-
+from .config import config
 def Challenge(challenge_id, API_KEY):
     if challenge_id in config['challenges'].keys():
         return config['challenges'][challenge_id]['class'](API_KEY, config)
